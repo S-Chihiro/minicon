b0649b254a6c4ea3cd95c5439ad6b3c091b92b61
“S-Chihiro” <36615094+S-Chihiro@users.noreply.github.com>
2018-02-22 06:56:46 +0000

    Create event controller, model and some views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,31 @@
 Rails.application.routes.draw do
+  # namespace :admin do
+  #   get 'events/index'
+  # end
+
+  # namespace :admin do
+  #   get 'events/show'
+  # end
+
+  # namespace :admin do
+  #   get 'events/new'
+  # end
+
+  # namespace :admin do
+  #   get 'events/edit'
+  # end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# http://guides.rubyonrails.org/routing.html
   # root 'controller_name#action_name'
   root 'application#hello'
   
   namespace :admin do
     resources :event_categories
+    resources :events
+    # get 'events', to: 'events#index'
+    # get 'events/show'
+    # get 'events/new'
+    # get 'events/edit'
   end
 end
---
+++ vendo/ruby/2.4.0/gems/nokogiri-1.8.1/ports/x86_64-pc-linux-gnu/libxml2/2.9.5/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/ec2-user/environment/minicon/vendo/ruby/2.4.0/gems/nokogiri-1.8.1/ports/x86_64-pc-linux-gnu/libxml2/2.9.5/lib"
+XML2_LIBS="-lxml2 -lz       -lm "
+XML2_INCLUDEDIR="-I/home/ec2-user/environment/minicon/vendo/ruby/2.4.0/gems/nokogiri-1.8.1/ports/x86_64-pc-linux-gnu/libxml2/2.9.5/include/libxml2"
+MODULE_VERSION="xml2-2.9.5"
+
